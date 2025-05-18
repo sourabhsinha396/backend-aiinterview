970e2c9cf332b00eafac308ae7db659f475d66e6
Sourabh Sinha <sourabhsinha396@gmail.com>
2025-05-18 18:35:53 +0530

    organize settings in config file

---
+++ core/config.py
@@ -0,0 +1,7 @@
+class Settings:
+    PROJECT_NAME: str = "Aiinterview API"
+    PROJECT_VERSION: str = "1.0.0"
+    PROJECT_DESCRIPTION: str = "APIs for the backend of Aiinterview"
+
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from core.config import settings
 
-app = FastAPI(title="API Chatbot", description="Chatbot API", version="1.0.0")
+
+app = FastAPI(title=settings.PROJECT_NAME, description=settings.PROJECT_DESCRIPTION, version=settings.PROJECT_VERSION)
 
 
 @app.get("/")
